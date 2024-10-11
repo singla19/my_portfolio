77a8ee6d06fae6b50d5f337cbba88021bd60a99d
singla19 <bhavaysingla2006@gmail.com>
2024-10-12 00:35:32 +0530

    Added resposiveness to contact section

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import './Contacts.css'
-import {MdOutlineEmail} from 'react-icons/md'
-import { useRef } from 'react';
+import React, { useRef } from 'react';
+import './Contacts.css';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const Contacts = () => {
-  const form = useRef()
+  const form = useRef();
 
   return (
     <section id='contacts'>
-
       <h5>Get in touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
-
           <article className='contact__option'>
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>bhavaysingla2006@gmail.com</h5>
-            <a href="mailto:thakshiladb2000@gmail.com">Send a message</a>
+            <a href="mailto:bhavaysingla2006@gmail.com">Send a message</a>
           </article>
         </div>
 
         <form ref={form}>
-
-         <input type="text" name="name" placeholder="Your Full Name" required />
-         <input type="email" name='email' placeholder='Your Email' required /> 
-         <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-         <button type="submit" className='btn btn-primary'>Send Message</button>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name='email' placeholder='Your Email' required /> 
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
       </div>    
     </section>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
